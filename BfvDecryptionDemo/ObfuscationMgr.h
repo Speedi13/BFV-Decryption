447e3f2fe34a3daaf560c49884ff1e1b4014453b
Speedi13 <Speedi13_im_web@web.de>
2019-01-30 02:05:01 +0100

    game update
    https://www.ea.com/games/battlefield/news/tides-of-war-chapter-2-lightning-strikes-update-notes-jan-29

--- BfvDecryptionDemo/ObfuscationMgr.h
+++ BfvDecryptionDemo/ObfuscationMgr.h
@@ -43,3 +43,5 @@ hashtable_iterator<_QWORD> *__fastcall hashtable_find(hashtable<_QWORD>* table, 
 
 fb::ClientPlayer* GetPlayerById( int id );
 fb::ClientPlayer* GetLocalPlayer( void );
+
+void TrickObfuscationMgr();
